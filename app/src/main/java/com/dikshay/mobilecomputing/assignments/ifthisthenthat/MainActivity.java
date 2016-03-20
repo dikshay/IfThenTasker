6490f73b19dbc88026f902730832a5efa3b32ef8
abdul140788 <abdulkalyaniwala@gmail.com>
2016-03-19 18:30:46 -0700

    GPS Change Tracking

--- app/src/main/java/com/dikshay/mobilecomputing/assignments/ifthisthenthat/MainActivity.java
+++ app/src/main/java/com/dikshay/mobilecomputing/assignments/ifthisthenthat/MainActivity.java
@@ -1,37 +1,51 @@
 package com.dikshay.mobilecomputing.assignments.ifthisthenthat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.dikshay.mobilecomputing.assignments.ifthisthenthat.service.GetLocationService;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button getLocation;
+    GetLocationService loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getLocation = (Button) findViewById(R.id.show_location);
+
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getCurrentContext(), GetLocationService.class);
+                startService(intent);
+                loc = new GetLocationService(MainActivity.this);
+                if (loc.canGetLocation()) {
+
+                    double latitude = loc.getLatitude();
+                    double longitude = loc.getLongitude();
+
+                    Toast.makeText(getApplicationContext(), "Dude, your location is - \n Lat: " +
+                            latitude + "\nLon: " + longitude, Toast.LENGTH_LONG).show();
+                }
+                else{
+                    loc.showSettingsAlert();
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private Context getCurrentContext(){
+        return getApplicationContext();
     }
 }
